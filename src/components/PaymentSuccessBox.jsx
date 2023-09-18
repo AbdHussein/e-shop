84c87f5e905e62aaeec17e7797c8e0b86e4545b6
farahmahdi <farahmahdi2001@gmail.com>
2023-09-18 14:31:50 +0300

    the changes u tell me to do :)

--- src/components/PaymentSuccessBox.jsx
+++ src/components/PaymentSuccessBox.jsx
@@ -1,22 +1,15 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
+import PaymentDetailed from './PaymentDetailed'
 
 const PaymentSuccessBox = ({orderNO,address,orderItem}) => {
   return (
     <Box sx={{maxWidth:"70%",display:"flex",flexDirection:"column",gap:"15px"}}>
        <Typography variant='h2'sx={{color:"#242424"}}>Payment Success !</Typography>
-       <div style={{display:"flex",justifyContent:"space-between"}}>
-        <Typography variant='h4'sx={{color:"#242424"}}>Order number</Typography>
-        <Typography variant='body2' sx={{color:"#707070"}}>{orderNO}</Typography>
-       </div>
-       <div style={{display:"flex",justifyContent:"space-between"}}>
-        <Typography variant='h4'sx={{color:"#242424"}}>Shipping Address</Typography>
-        <Typography variant='body2' sx={{color:"#707070"}}>{address}</Typography>
-       </div>
-       <div style={{display:"flex",justifyContent:"space-between"}}>
-        <Typography variant='h4'sx={{color:"#242424"}}>Order Items</Typography>
-        <Typography variant='body2' sx={{color:"#707070"}}>{orderItem}</Typography>
-       </div>
+       <PaymentDetailed title={"Order number"} value={orderNO}/>
+       <PaymentDetailed title={"Shipping Address"} value={address}/>
+       <PaymentDetailed title={"Order Items"} value={orderItem}/>
+     
        <Typography variant='body2'sx={{color:"#242424",textWrap:"wrap",maxWidth:"382px"}}>An email will be sent to your email address contains order confirmation and tacking code.</Typography>
     </Box>
   )
